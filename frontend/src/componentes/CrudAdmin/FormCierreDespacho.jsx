5c7b347bf2849839b37c78f2483b52916ac5a6bc
victorlinares100 <linares28072004@gmail.com>
2026-05-23 22:06:57 -0400

    deploy

--- frontend/src/componentes/CrudAdmin/FormCierreDespacho.jsx
+++ frontend/src/componentes/CrudAdmin/FormCierreDespacho.jsx
@@ -16,7 +16,7 @@ export const FormCierreDespacho = ({ despacho, onClose }) => {
 
     try {
       await axios.put(
-        `http://10.0.13.173/api/v1/despachos/${despacho.idDespacho}`,
+        `/api/v1/despachos/${despacho.idDespacho}`,
         jsonData,
         {
           headers:{
--- frontend/src/componentes/CrudAdmin/TableDespachos.jsx
+++ frontend/src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://10.0.13.173/api/v1/despachos/", {
+      .get("/api/v1/despachos/", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
