e98073c41dae838b3dfa639476fd94b1b68d0d54
victorlinares100 <linares28072004@gmail.com>
2026-07-12 16:02:55 -0400

    deploy con k8s

--- frontend/src/componentes/CrudAdmin/PruebaCards.jsx
+++ frontend/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra aaaaaaaaaaaa"
+          title="Consultar Ordenes de compra aaaaaaaaaaaaa"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
