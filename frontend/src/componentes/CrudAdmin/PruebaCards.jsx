a57a69eedef1f804aeb71de32b0f92ce7f886e17
victorlinares100 <linares28072004@gmail.com>
2026-07-12 21:16:32 -0400

    deploy

--- frontend/src/componentes/CrudAdmin/PruebaCards.jsx
+++ frontend/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra aaaaaaaaaaaaa"
+          title="Consultar Ordenes de compra a"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
